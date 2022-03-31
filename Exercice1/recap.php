8aa95f8937b92a2a7aeb1380cf47168532162433
Habib Sellami <habib.sellami@outlook.com>
2022-03-31 19:16:15 +0100

    add file upload option

--- Exercice1/recap.php
+++ Exercice1/recap.php
@@ -9,14 +9,35 @@
     <link href="style.css" rel="stylesheet">
 </head>
 <body style="flex-direction: column">
-<h3>Bonjour  <?php printf( "%s %s",$_POST['prenom'] , $_POST['nom'])?>
-</h3>
-<h3>Prix totale : </h3> <?php
-$nbSandwich = $_POST['nbSandwich'];
+<?php
+
+
+
+$file_name_cmps = explode('.',$_FILES['CIN']['name']);
+$file_extension = strtolower(end($file_name_cmps));
+$target_dir = "./Uploads/";
+$target_file = $target_dir . "CIN" . '.'.$file_extension;
+$tempname = $_FILES['CIN']["tmp_name"];
+
+$prenom = htmlspecialchars($_POST['prenom']);
+$nom = htmlspecialchars($_POST['nom']);
+$nbSandwich = intval(htmlspecialchars($_POST['nbSandwich']));
+
 $prix = $nbSandwich * 4;
 if ($nbSandwich >= 10)
     $prix = $prix * 0.9;
-echo " <b>$prix dt</b>";
+
+move_uploaded_file($tempname, $target_file);
+
 ?>
+
+<h3>Bonjour <?= $prenom ?> <?= $nom ?></h3>
+
+<h3>Prix totale : </h3>
+<b><?= $prix ?> DT</b>
+
+<h1>Votre CIN : </h1>
+<img src="Uploads/CIN.<?=$file_extension?>">
+
 </body>
 </html>
